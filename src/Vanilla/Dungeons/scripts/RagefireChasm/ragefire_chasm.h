c85e3b80de22732ecddf7602783747210ad31879
FlyingArowana <AugustoMendes4426@gmail.com>
2024-11-04 12:32:01 +0000

    RFC copied with the Changes

---
+++ src/Vanilla/Dungeons/scripts/RagefireChasm/ragefire_chasm.h
@@ -0,0 +1,27 @@
+#ifndef DEF_RFC_H
+#define DEF_RFC_H
+
+#include "CreatureAIImpl.h"
+
+#define DataHeader "RFC"
+
+#define RagefireChasmScriptName "instance_ragefire_chasm"
+
+enum DataTypes
+{
+    DATA_OGGLEFLINT = 0,
+    DATA_TARAGAMAN_THE_HUNGERER = 1,
+    DATA_BAZZALAN = 2,
+    DATA_JERGOSH_THE_INVOKER = 3,
+    MAX_ENCOUNTERS = 4,
+};
+
+enum CreaturesIds
+{
+    NPC_OGGLEFLINT = 11517,
+    NPC_TARAGAMAN_THE_HUNGERER = 11520,
+    NPC_BAZZALAN = 11519,
+    NPC_JERGOSH_THE_INVOKER = 11518,
+};
+
+#endif
